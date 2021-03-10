d70d10fcdc8864822ddc6cf4cc6cecbdee1d9ae8
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2021-03-10 23:49:07 +0100

    Fix pvp f2P and p2p units

--- src/app/data/tier-lists/pvp.ts
+++ src/app/data/tier-lists/pvp.ts
@@ -1,5 +1,5 @@
-export const gloPvps = ['2934', '2989', '3016'];
+export const gloPvps = ['2934', '2989', '3016', '3047', '3082', '3107'];
 
-export const japPvps = ['3047', '3082', '3107', '3142', '3173', '3251'];
+export const japPvps = ['3142', '3173', '3208', '3233', '3251'];
 
 export const pvps = [...gloPvps, ...japPvps];
